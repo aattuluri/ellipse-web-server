a246947c23262146c3d77daf95db8a82a035c38f
aattuluri <44482891+aattuluri@users.noreply.github.com>
2020-08-24 22:03:59 -0700

    Update install_node_utils.sh

--- scripts/install_node_utils.sh
+++ scripts/install_node_utils.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-echo "Y" | sudo apt install nodejs
+curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.35.3/install.sh -o install_nvm.sh
+bash install_nvm.sh
+source ~/.profile
+nvm install 12.18.3
 echo "Y" | install npm
-nodejs -v
 npm install pm2 -g
