a2e31fbe575e7d6fa8a7463dd1fbe67d754c4122
aattuluri <44482891+aattuluri@users.noreply.github.com>
2020-08-25 00:27:31 -0700

    Update deploy_remote.sh

--- scripts/deploy_remote.sh
+++ scripts/deploy_remote.sh
@@ -4,29 +4,16 @@ BUILD_ID=`date +%s`
 DEPLOY_ROOT="/srv/ellipseapp"
 BUILD_DIR="$DEPLOY_ROOT/$BUILD_ID"
 APP_DIR="$BUILD_DIR/ellipse-web-server"
-
-MODULES_CACHE_WEB="$DEPLOY_ROOT/node_modules_ui.tar"
-MODULES_CACHE_ELLIPSEWEBSERVER="$DEPLOY_ROOT/node_modules_ellipse-web-server.tar"
+UI_DIR="$BUILD_DIR/ellipse-web-ui"
 
 SCRIPT_PWD=`pwd`
-
-#function build_ui {
-  #TODO
-  #echo "Building UI"
-  #cd $BUILD_DIR
-  #sudo -u deploy tar xf $MODULES_CACHE_WEB
-  #sudo -u deploy ln -s /srv/ellipseapp/localConfig.json $BUILD_DIR/localConfig.json
-  #sudo -u deploy npm prune
-  #sudo -u deploy npm install
-  #sudo -u deploy npm install string-format moment-timezone html-pdf
-  #sudo -u deploy tar cf $MODULES_CACHE_WEB node_modules
-#}
 
 function build_ellipse_web_server {
   echo "Building ellipseapp web server"
   cd $APP_DIR
   npm prune
   npm install
+  npm build
 }
 
 cd $DEPLOY_ROOT
@@ -35,14 +22,20 @@ cd $DEPLOY_ROOT
 mkdir -p $APP_DIR
 git clone --branch master --depth 1 git@github.com:aattuluri/ellipse-web-server $APP_DIR
 
+mkdir -p $UI_DIR
+git clone --branch master --depth 1 git@github.com:aattuluri/ellipse-web-ui $UI_DIR
+
 # Build artifacts.
-#build_ui
 build_ellipse_web_server
 
-# Restart node process.
+# Restart nodejs web server process.
 cd $APP_DIR
 pm2 delete app
 pm2 start app.js
+
+# Copy ui build
+cd $UI_DIR
+cp -R ./build/* /var/www/$DOMAIN_NAME/html
 
 # Update current symlink
 rm /srv/ellipseapp/current
