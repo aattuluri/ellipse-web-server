4fd82a677c51934a73f887a88c87c86830f9bcf0
aattuluri <44482891+aattuluri@users.noreply.github.com>
2020-08-25 00:54:19 -0700

    Update install_mongo.sh

--- scripts/install_mongo.sh
+++ scripts/install_mongo.sh
@@ -2,3 +2,6 @@
 echo "Y" | sudo apt install -y mongodb
 sudo systemctl status mongodb
 mongo --eval 'db.runCommand({ connectionStatus: 1 })'
+sudo sed -i 's/#auth\s=\strue/auth = true/g' /etc/mongodb.conf
+sudo systemctl restart mongodb
+mongo --eval 'db.runCommand({ connectionStatus: 1 })'
