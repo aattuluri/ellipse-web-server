b9c6a2b98ab9f36cc3c3173d40d8b8ab90e35e3e
aattuluri <44482891+aattuluri@users.noreply.github.com>
2020-10-17 21:11:14 -0700

    Update nginx_conf.sh

--- scripts/nginx_conf.sh
+++ scripts/nginx_conf.sh
@@ -8,12 +8,12 @@ sudo chown -R root:root /etc/nginx/ssl
 sudo chmod -R 600 /etc/nginx/ssl
 sudo cp ./nginx/certs.conf /etc/nginx/snippets/
 sudo cp ./nginx/ssl-params.conf /etc/nginx/snippets/
-sudo openssl dhparam -out /etc/nginx/ssl/dhparam.pem 2048;
+sudo openssl dhparam -out /etc/nginx/dhparam.pem 2048;
 #Copy certs/key to /etc/nginx/ssl
 echo "Y" | sudo apt install unzip
 unzip $DOMAIN_NAME.zip
 sudo cat certificate.crt >> /etc/nginx/ssl/nginx-ca-bundle.crt
-sudo cat "\n" >> /etc/nginx/ssl/nginx-ca-bundle.crt
+sudo echo "" >> /etc/nginx/ssl/nginx-ca-bundle.crt
 sudo cat ca_bundle.crt >> /etc/nginx/ssl/nginx-ca-bundle.crt
-sudo cp private.key >> /etc/nginx/ssl/nginx-key.key
+sudo cp private.key /etc/nginx/ssl/nginx-key.key
 sudo nginx -t && sudo systemctl restart nginx
