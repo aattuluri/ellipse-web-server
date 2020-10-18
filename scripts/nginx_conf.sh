b59c1522770ba59722917c6a888abfb29dd24054
aattuluri <44482891+aattuluri@users.noreply.github.com>
2020-10-17 19:29:03 -0700

    Update nginx_conf.sh

--- scripts/nginx_conf.sh
+++ scripts/nginx_conf.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo cp ./nginx/$DOMAIN_NAME /etc/nginx/sites-available/
 sudo ln -s /etc/nginx/sites-available/$DOMAIN_NAME /etc/nginx/sites-enabled/
+sudo cp ./nginx/admin.$DOMAIN_NAME /etc/nginx/sites-available/
+sudo ln -s /etc/nginx/sites-available/admin.$DOMAIN_NAME /etc/nginx/sites-enabled/
 sudo mkdir /etc/nginx/ssl
 sudo chown -R root:root /etc/nginx/ssl
 sudo chmod -R 600 /etc/nginx/ssl
