75bec44fa3c7549ff8bc6ec4b0888a3cf60d7e3d
Lalith <lalithpunepalli@gmail.com>
2020-12-25 19:27:26 +0530

    submission for rounds added

--- Routers/SubmissionRoute.js
+++ Routers/SubmissionRoute.js
@@ -39,7 +39,23 @@ router.post('/api/event/team/add_submission', auth, async (req, res) => {
 
         }
         else {
-
+            const registration = await Registration.findOne({_id: req.body.reg_id});
+            var sub = registration.submissions;
+            var updatedSubs = [];
+            sub.forEach((s, index) => {
+                console.log(s);
+                if (req.body.event_round == s.title) {
+                    updatedSubs.push({ 'title': s.title, 'type': s.type, is_submitted: true, submission_access: s.submission_access, submission_id: submission._id })
+                }
+                else {
+                    updatedSubs.push(s);
+                }
+                if (index + 1 == sub.length) {
+                    Registration.updateOne({ _id: req.body.reg_id }, { $set: { submissions: updatedSubs } }).then((value) => {
+                        res.status(200).json({ "message": "success" });
+                    })
+                }
+            });
         }
 
 
@@ -48,6 +64,38 @@ router.post('/api/event/team/add_submission', auth, async (req, res) => {
         res.status(400).json({ error: error.message })
     }
 })
+
+
+router.post('/api/event/team/edit_submission', auth, async (req, res) => {
+    try {
+        const user = req.user;
+        await Submission.updateOne({_id: req.body.sub_id},{$set:{submission:req.body.submission}});
+        res.status(200).json({"message": "success"})
+    }
+    catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+})
+
+//get submission
+router.get('/api/event/get_submission',auth,async (req,res)=>{
+    try{
+        const submission = await Submission.findOne({_id: req.query.id});
+        if(submission){
+            res.status(200).json(submission);
+        }
+        else{
+            res.status(400).json({message: "not_found"})
+        }
+        
+
+    }
+    catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+})
+
+
 
 
 module.exports = router
